28e6d303f3bf5032326d3639b11225c44317f677
CMiller <mlr.chase40@gmail.com>
2022-08-04 20:52:45 -0500

    add pwa capabilities by adding service worker and manifest.json

---
+++ public/service-worker.js
@@ -0,0 +1,63 @@
+const FILES_TO_CACHE = [
+  './index.html',
+  './css/styles.css',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+  './js/index.js',
+  './js/idb.js',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png'
+];
+
+const APP_PREFIX = 'BudgetU-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeepList = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeepList.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeepList.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  console.log('fetching : ' + event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        console.log('returning from cache : ' + event.request.url);
+        return response;
+      } else {
+        console.log('file is not cached, fetching : ' + event.request.url);
+        return fetch(event.request);
+      }
+    })
+  );
+});
